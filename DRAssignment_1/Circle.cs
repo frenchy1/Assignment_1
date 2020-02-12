145af53d6b1b128fe1de449e428c9022d4fcd304
Dave Ratte <englishgamer123@gmail.com>
2020-02-11 19:59:03 -0500

    Circle class commit

---
+++ DRAssignment_1/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRAssignment_1
+{
+    public class Circle
+    {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int GetRadius()
+        {
+            return this.radius;
+        }
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+        public double GetCircumference()
+        {
+            return this.radius * 2 * Math.PI;
+        }
+        public double GetArea()
+        {
+            return this.radius * radius * Math.PI;
+        }
+
+    }
+
+}
